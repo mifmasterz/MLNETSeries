35a916bf2f1c7950db737e2505c1f3e6935eb386
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-07-31 06:51:40 +0700

    update

---
+++ AutoML_ModelBuilder/AutoML_ModelBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoML_ModelBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
