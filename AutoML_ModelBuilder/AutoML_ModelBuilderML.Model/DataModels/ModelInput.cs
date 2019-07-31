abe6ba2b197931ee5b7843b63b1da27a1ee2eb96
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-07-31 08:08:51 +0700

    update

---
+++ AutoML_ModelBuilder/AutoML_ModelBuilderML.Model/DataModels/ModelInput.cs
@@ -0,0 +1,22 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using Microsoft.ML.Data;
+
+namespace AutoML_ModelBuilderML.Model.DataModels
+{
+    public class ModelInput
+    {
+        [ColumnName("CONTENT"), LoadColumn(0)]
+        public string CONTENT { get; set; }
+
+
+        [ColumnName("CLASS"), LoadColumn(1)]
+        public bool CLASS { get; set; }
+
+
+    }
+}
