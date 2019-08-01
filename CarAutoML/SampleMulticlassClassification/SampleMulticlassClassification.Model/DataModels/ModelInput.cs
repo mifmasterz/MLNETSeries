2ace5cd104ff820c4a73bed36f056c26e4e16441
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-08-01 09:20:49 +0700

    update

---
+++ CarAutoML/SampleMulticlassClassification/SampleMulticlassClassification.Model/DataModels/ModelInput.cs
@@ -0,0 +1,42 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using Microsoft.ML.Data;
+
+namespace SampleMulticlassClassification.Model.DataModels
+{
+    public class ModelInput
+    {
+        [ColumnName("col0"), LoadColumn(0)]
+        public string Col0 { get; set; }
+
+
+        [ColumnName("col1"), LoadColumn(1)]
+        public string Col1 { get; set; }
+
+
+        [ColumnName("col2"), LoadColumn(2)]
+        public string Col2 { get; set; }
+
+
+        [ColumnName("col3"), LoadColumn(3)]
+        public string Col3 { get; set; }
+
+
+        [ColumnName("col4"), LoadColumn(4)]
+        public string Col4 { get; set; }
+
+
+        [ColumnName("col5"), LoadColumn(5)]
+        public string Col5 { get; set; }
+
+
+        [ColumnName("Label"), LoadColumn(6)]
+        public string Label { get; set; }
+
+
+    }
+}
