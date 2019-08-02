c4526f0cc1e8d745bd80a1cc5636a00b8f95434a
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-08-03 06:36:24 +0700

    update

---
+++ TrafficVolumeAutoMLAPI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
+
+namespace TrafficVolumeAutoMLAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            IDataView trainDataView = mlContext.Data.LoadFromTextFile<TrafficData>(GetAbsolutePath("../../../Data/Metro_Interstate_Traffic_Volume.csv"), hasHeader: true,separatorChar:',');
+            //configure experiment settings
+            var experimentSettings = new RegressionExperimentSettings();
+            experimentSettings.MaxExperimentTimeInSeconds = 10;
+            var cts = new CancellationTokenSource();
+            experimentSettings.CancellationToken = cts.Token;
+            experimentSettings.OptimizingMetric = RegressionMetric.MeanSquaredError;
+            experimentSettings.CacheDirectory = null;
+            
+            // Cancel experiment after the user presses any key
+            CancelExperimentAfterAnyKeyPress(cts);
+            //create experiment
+            RegressionExperiment experiment = mlContext.Auto().CreateRegressionExperiment(experimentSettings);
+            var handler = new RegressionExperimentProgressHandler();
+            //execute experiment
+            ExperimentResult<RegressionMetrics> experimentResult = experiment.Execute(trainDataView,   labelColumnName: "Label", progressHandler:handler);
+            //Evaluate
+            RegressionMetrics metrics = experimentResult.BestRun.ValidationMetrics;
+            Console.WriteLine($"Best Algorthm: {experimentResult.BestRun.TrainerName}");
+            Console.WriteLine($"R-Squared: {metrics.RSquared:0.##}");
+            Console.WriteLine($"Root Mean Squared Error: {metrics.RootMeanSquaredError:0.##}");
+            Console.ReadKey();
+        }
+
+        static async void CancelExperimentAfterAnyKeyPress(CancellationTokenSource cts)
+        {
+            Task.Run(() => {
+                while (true)
+                {
+                    Console.ReadKey();
+                    cts.Cancel();
+                }
+            });
+        }
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+            return fullPath;
+        }
+    }
+}
---
+++ TrafficVolumeAutoMLAPI/TrafficData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrafficVolumeAutoMLAPI
+{
+    class TrafficData
+    {
+        [LoadColumn(0), ColumnName("holiday")]
+        public string holiday { get; set; }
+
+        [LoadColumn(1), ColumnName("temp")]
+
+        public float temp { get; set; }
+
+        [LoadColumn(2), ColumnName("rain")]
+
+        public float rain { get; set; }
+
+        [LoadColumn(3), ColumnName("snow")]
+
+        public float snow { get; set; }
+
+        [LoadColumn(4), ColumnName("cloud")]
+
+        public float cloud { get; set; }
+
+        [LoadColumn(5), ColumnName("weather")]
+
+        public string weather { get; set; }
+
+        [LoadColumn(6), ColumnName("weather_desc")]
+
+        public string weather_desc { get; set; }
+
+       
+
+        [LoadColumn(7), ColumnName("Label")]
+
+        public float traffic_volume { get; set; }
+
+    }
+}
