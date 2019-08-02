85ec99db742675c6606f46f30fa9869461d6bfc4
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-08-03 06:55:12 +0700

    update

--- TrafficVolumeAutoMLAPI/TrafficData.cs
+++ TrafficVolumeAutoMLAPI/TrafficData.cs
@@ -11,33 +11,24 @@ namespace TrafficVolumeAutoMLAPI
         public string holiday { get; set; }
 
         [LoadColumn(1), ColumnName("temp")]
-
         public float temp { get; set; }
 
         [LoadColumn(2), ColumnName("rain")]
-
         public float rain { get; set; }
 
         [LoadColumn(3), ColumnName("snow")]
-
         public float snow { get; set; }
 
         [LoadColumn(4), ColumnName("cloud")]
-
         public float cloud { get; set; }
 
         [LoadColumn(5), ColumnName("weather")]
-
         public string weather { get; set; }
 
         [LoadColumn(6), ColumnName("weather_desc")]
-
         public string weather_desc { get; set; }
 
-       
-
         [LoadColumn(7), ColumnName("Label")]
-
         public float traffic_volume { get; set; }
 
     }
